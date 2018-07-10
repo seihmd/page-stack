bfb2d802a999e327e462d6eee1e84f027ee543b4
seihmd <moku.said@gmail.com>
2018-07-10 23:48:22 +0900

    history api

--- index.js
+++ index.js
@@ -1,61 +1,87 @@
 const defaultOption = {
-  baseElement: 'main-content',
   showClass: 'show'
 };
 
+// TODO should work without history
 class PageStack {
   /**
+   * @param {string} baseId
    * @param {Object} [option={}]
    */
-  constructor(option = {}) {
+  constructor(baseId, option = {}) {
+    const baseEl = document.getElementById(baseId);
+    if (!baseEl) {
+      throw Error(`element with id: ${baseId} is not found`);
+    }
     this.option = Object.assign(defaultOption, option);
-    this.stack = [this.option.baseElement];
+    this.baseId = baseId;
+    this.currentId = baseId;
+    this.memo = {
+      [baseId]: {
+        scroll: 0,
+        el: baseEl
+      }
+    };
+
+    window.history.pushState({ id: this.baseId }, '');
+    window.onpopstate = ({ state }) => {
+      if (state && state.id) {
+        this.push(state.id, false);
+      }
+    };
+  }
+
+  pushBase() {
+    if (this.baseId === this.currentId) {
+      return;
+    }
+
+    this.push(this.baseId);
+  }
+
+  push(id, pushHistory = true) {
+    this.memoId(id);
+    this.hide();
+    this.show(id);
+    if (pushHistory) {
+      window.history.pushState({ id: this.currentId }, '');
+    }
   }
 
   pop() {
-    if (this.stack.length === 1) {
-      return;
-    }
-    const current = this.stack.pop();
-    this.hide(current);
-    this.show(this.stack[this.stack.length - 1]);
+    window.history.back();
   }
 
-  push(id) {
-    const current = this.stack[this.stack.length - 1];
-    if (current === id) {
-      return;
-    }
-    this.stack.push(id);
-    this.hide(current);
-    this.show(id);
-  }
-
-  replace(id) {
-    const current = this.stack[this.stack.length - 1];
-    if (current === id) {
-      return;
-    }
-    this.stack[this.stack.length - 1] = id;
-    this.hide(current);
-    this.show(id);
-  }
-
-  toggle(id, toggleToShow) {
-    const target = window.document.getElementById(id);
-    target.classList.toggle(this.option.showClass, toggleToShow);
-  }
-
-  hide(id) {
-    if (typeof id === 'string') {
-      this.toggle(id, false);
-    }
+  hide() {
+    this.memo[this.currentId].scroll = window.scrollY;
+    this.toggle(this.currentId, false);
   }
 
   show(id) {
-    if (typeof id === 'string') {
-      this.toggle(id, true);
+    this.toggle(id, true);
+    // TODO 必要？
+    // window.scrollTo(0, this.memo[id].scroll);
+    this.currentId = id;
+  }
+
+  toggle(id, toggleToShow) {
+    const { el } = this.memo[id];
+    el.classList.toggle(this.option.showClass, toggleToShow);
+  }
+
+  memoId(id) {
+    if (this.memo[id]) {
+      return;
     }
+
+    const el = document.getElementById(id);
+    if (!el) {
+      throw Error(`element with id: ${id} is not found`);
+    }
+    this.memo[id] = {
+      scroll: 0,
+      el
+    };
   }
 }
 
